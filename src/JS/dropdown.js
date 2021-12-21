4a258cd67c2b53f02bd4a65356b090339a7198ce
SONGDAM <herb7761@gmail.com>
2021-12-22 03:08:24 +0900

    menu and basic layout

---
+++ src/JS/dropdown.js
@@ -0,0 +1,25 @@
+"use strict"; 
+
+const menu = document.querySelector(".button");
+const dropdowns = document.querySelectorAll(".main-menu");
+
+menu.addEventListener("click",()=>{
+    for(const dropdown of dropdowns){
+        dropdown.style.display="block";
+    }
+});
+
+
+
+    
+       
+menu.addEventListener("blur",()=>{
+     for(const dropdown of dropdowns){
+        dropdown.style.display="";
+    }
+
+    setTimeout(()=>{
+        dropdown.style.display="";
+    },2000);
+});
+
