c1bfb026fe53714fe0dfc35e296f99114e1e2348
SONGDAM <herb7761@gmail.com>
2021-12-29 23:42:11 +0900

    modified menu logic

--- src/JS/dropdown.js
+++ src/JS/dropdown.js
@@ -1,19 +1,11 @@
-"use strict"; 
+"use strict";
 
 const menu = document.querySelector(".button");
 const dropdowns = document.querySelectorAll(".main-menu");
 
-
-// menu.addEventListener("click",()=>{
-//     for(const dropdown of dropdowns){
-//         dropdown.style.display="block";
-//         console.log("")
-//     }
-// });
-
-menu.addEventListener("click",(e)=>{
-    menu.classList.toggle("show")
-})
-
-    
-       
+menu.addEventListener("click", function (e) {
+    for (const dropdown of dropdowns) {
+        dropdown.classList.toggle("show")
+        console.log("")
+    }
+});
