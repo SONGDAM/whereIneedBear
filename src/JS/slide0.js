39e378e590debf2b8470732b9ec6d6c234f92977
SONGDAM <herb7761@gmail.com>
2022-01-11 20:15:36 +0900

    add scroll function

--- src/JS/slide0.js
+++ src/JS/slide0.js
@@ -1,39 +1,39 @@
-let curPos= 0;
+let curPos = 0;
 let position = 0;
 const IMAGE_WIDTH = 1280;
 
-const prevBtn = document.querySelector(".prev")
-const nextBtn = document.querySelector(".next")
-const images = document.querySelector(".images")
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+const images = document.querySelector('.images');
 
-function prev(){
-    if(curPos > 0){
-    nextBtn.removeAttribute("disabled")
-    position += IMAGE_WIDTH
+function prev() {
+	if (curPos > 0) {
+		nextBtn.removeAttribute('disabled');
+		position += IMAGE_WIDTH;
 
-    images.style.transform = `translateX(${position}px)`
-    curPos -= 1;
-    }
-    if(curPos == 0){
-        prevBtn.setAttribute("disabled", 'true')
-    }
- }
-function next(){
-    if(curPos < 2){
-        prevBtn.removeAttribute("disabled")
-        position -= IMAGE_WIDTH
-
-        images.style.transform = `translateX(${position}px)`
-        curPos += 1;
-    }
-    if(curPos == 2){
-        nextBtn.setAttribute("disabled", 'true')
-    }
+		images.style.transform = `translateX(${position}px)`;
+		curPos -= 1;
+	}
+	if (curPos == 0) {
+		prevBtn.setAttribute('disabled', 'true');
+	}
 }
-function init(){
-    prevBtn.setAttribute("disabled", 'true')
-    prevBtn.addEventListener("click", prev)
-    nextBtn.addEventListener("click", next)
+function next() {
+	if (curPos < 2) {
+		prevBtn.removeAttribute('disabled');
+		position -= IMAGE_WIDTH;
+
+		images.style.transform = `translateX(${position}px)`;
+		curPos += 1;
+	}
+	if (curPos == 2) {
+		nextBtn.setAttribute('disabled', 'true');
+	}
+}
+function init() {
+	prevBtn.setAttribute('disabled', 'true');
+	prevBtn.addEventListener('click', prev);
+	nextBtn.addEventListener('click', next);
 }
 
 init();
