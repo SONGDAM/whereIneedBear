0971df0ebd8f919f9130bc00a4f52c33b5fb0d0a
SONGDAM <herb7761@gmail.com>
2022-01-23 13:08:37 +0900

    merge

--- src/JS/my-img.js
+++ src/JS/my-img.js
@@ -1,358 +1,534 @@
-const items = [{
-    name: "4_img_1",
-    temperature: "4",
-    image: "../assets/attire/4_img_1.jpg",
-  },
-  {
-    name: "4_img_2",
-    temperature: "4",
-    image: "../assets/attire/4_img_2.jpg",
-  },
-  {
-    name: "4_img_3",
-    temperature: "4",
-    image: "../assets/attire/4_img_3.jpg",
-  },
-  {
-    name: "4_img_4",
-    temperature: "4",
-    image: "../assets/attire/4_img_4.jpg",
-  },
-  {
-    name: "4_img_5",
-    temperature: "4",
-    image: "../assets/attire/4_img_5.jpg",
-  },
-  {
-    name: "4_img_6",
-    temperature: "4",
-    image: "../assets/attire/4_img_6.jpg",
-  },
-  {
-    name: "4_img_7",
-    temperature: "4",
-    image: "../assets/attire/4_img_7.jpg",
-  },
-  {
-    name: "4_img_8",
-    temperature: "4",
-    image: "../assets/attire/4_img_8.jpg",
-  },
-  {
-    name: "4_img_9",
-    temperature: "4",
-    image: "../assets/attire/4_img_9.jpg",
-  },
-  {
-    name: "4_img_10",
-    temperature: "4",
-    image: "../assets/attire/4_img_10.jpg",
-  },
-  {
-    name: "4_img_11",
-    temperature: "4",
-    image: "../assets/attire/4_img_11.jpg",
-  },
-  {
-    name: "4_img_12",
-    temperature: "4",
-    image: "../assets/attire/4_img_12.jpg",
-  },
-  {
-    name: "4_img_13",
-    temperature: "4",
-    image: "../assets/attire/4_img_13.jpg",
-  },
-  {
-    name: "4_img_14",
-    temperature: "4",
-    image: "../assets/attire/4_img_14.jpg",
-  },
-  {
-    name: "4_img_15",
-    temperature: "4",
-    image: "../assets/attire/4_img_15.jpg",
-  },
-  {
-    name: "4_img_16",
-    temperature: "4",
-    image: "../assets/attire/4_img_16.jpg",
-  },
-  {
-    name: "5_img_1",
-    temperature: "5",
-    image: "../assets/attire/5_img_1.jpg",
-  },
-  {
-    name: "5_img_2",
-    temperature: "5",
-    image: "../assets/attire/5_img_2.jpg",
-  },
-  {
-    name: "5_img_3",
-    temperature: "5",
-    image: "../assets/attire/5_img_3.jpg",
-  },
-  {
-    name: "5_img_4",
-    temperature: "5",
-    image: "../assets/attire/5_img_4.jpg",
-  },
-  {
-    name: "5_img_5",
-    temperature: "5",
-    image: "../assets/attire/5_img_5.jpg",
-  },
-  {
-    name: "5_img_6",
-    temperature: "5",
-    image: "../assets/attire/5_img_6.jpg",
-  },
-  {
-    name: "5_img_7",
-    temperature: "5",
-    image: "../assets/attire/5_img_7.jpg",
-  },
-  {
-    name: "5_img_8",
-    temperature: "5",
-    image: "../assets/attire/5_img_8.jpg",
-  },
-  {
-    name: "5_img_9",
-    temperature: "5",
-    image: "../assets/attire/5_img_9.jpg",
-  },
-  {
-    name: "5_img_10",
-    temperature: "5",
-    image: "../assets/attire/5_img_10.jpg",
-  },
-  {
-    name: "5_img_11",
-    temperature: "5",
-    image: "../assets/attire/5_img_11.jpg",
-  },
-  {
-    name: "5_img_12",
-    temperature: "5",
-    image: "../assets/attire/5_img_12.jpg",
-  },
-  {
-    name: "5_img_13",
-    temperature: "5",
-    image: "../assets/attire/5_img_13.jpg",
-  },
-  {
-    name: "5_img_14",
-    temperature: "5",
-    image: "../assets/attire/5_img_14.jpg",
-  },
-  {
-    name: "5_img_15",
-    temperature: "5",
-    image: "../assets/attire/5_img_15.jpg",
-  },
-  {
-    name: "5_img_16",
-    temperature: "5",
-    image: "../assets/attire/5_img_16.jpg",
-  },
-  {
-    name: "9_img_1",
-    temperature: "9",
-    image: "../assets/attire/9_img_1.jpg",
-  },
-  {
-    name: "9_img_2",
-    temperature: "9",
-    image: "../assets/attire/9_img_2.jpg",
-  },
-  {
-    name: "9_img_3",
-    temperature: "9",
-    image: "../assets/attire/9_img_3.jpg",
-  },
-  {
-    name: "9_img_4",
-    temperature: "9",
-    image: "../assets/attire/9_img_4.jpg",
-  },
-  {
-    name: "9_img_5",
-    temperature: "9",
-    image: "../assets/attire/9_img_5.jpg",
-  },
-  {
-    name: "9_img_6",
-    temperature: "9",
-    image: "../assets/attire/9_img_6.jpg",
-  },
-  {
-    name: "9_img_7",
-    temperature: "9",
-    image: "../assets/attire/9_img_7.jpg",
-  },
-  {
-    name: "9_img_8",
-    temperature: "9",
-    image: "../assets/attire/9_img_8.jpg",
-  },
-  {
-    name: "9_img_9",
-    temperature: "9",
-    image: "../assets/attire/9_img_9.jpg",
-  },
-  {
-    name: "9_img_10",
-    temperature: "9",
-    image: "../assets/attire/9_img_10.jpg",
-  },
-  {
-    name: "9_img_11",
-    temperature: "9",
-    image: "../assets/attire/9_img_11.jpg",
-  },
-  {
-    name: "9_img_12",
-    temperature: "9",
-    image: "../assets/attire/9_img_12.jpg",
-  },
-  {
-    name: "9_img_13",
-    temperature: "9",
-    image: "../assets/attire/9_img_13.jpg",
-  },
-  {
-    name: "9_img_14",
-    temperature: "9",
-    image: "../assets/attire/9_img_14.jpg",
-  },
-  {
-    name: "9_img_15",
-    temperature: "9",
-    image: "../assets/attire/9_img_15.jpg",
-  },
-  {
-    name: "9_img_16",
-    temperature: "9",
-    image: "../assets/attire/9_img_16.jpg",
-  },
+const items = [
+	{
+		name: '4_1_c',
+		temperature: '-4',
+		look: 'casual',
+		image: './assets/attire/4_1_c.jpg',
+	},
+	{
+		name: '4_2_c',
+		temperature: '-4',
+		look: 'casual',
+		image: './assets/attire/4_2_c.jpg',
+	},
+	{
+		name: '4_3_m',
+		temperature: '-4',
+		look: 'modern',
+		image: './assets/attire/4_3_m.jpg',
+	},
+	{
+		name: '4_4_m',
+		temperature: '4',
+		look: 'modern',
+		image: './assets/attire/4_4_m.jpg',
+	},
+	{
+		name: '4_5_s',
+		temperature: '4',
+		look: 'street',
+		image: './assets/attire/4_5_s.jpg',
+	},
+	{
+		name: '4_6_c',
+		temperature: '4',
+		look: 'casual',
+		image: './assets/attire/4_6_c.jpg',
+	},
+	{
+		name: '4_7_c',
+		temperature: '4',
+		look: 'casual',
+		image: './assets/attire/4_7_c.jpg',
+	},
+	{
+		name: '4_8_c',
+		temperature: '4',
+		look: 'casual',
+		image: './assets/attire/4_8_c.jpg',
+	},
+	{
+		name: '4_9_c',
+		temperature: '4',
+		look: 'casual',
+		image: './assets/attire/4_9_c.jpg',
+	},
+	{
+		name: '4_10_c',
+		temperature: '4',
+		look: 'casual',
+		image: './assets/attire/4_10_c.jpg',
+	},
+	{
+		name: '4_11_c',
+		temperature: '4',
+		look: 'casual',
+		image: './assets/attire/4_11_c.jpg',
+	},
+	{
+		name: '4_12_m',
+		temperature: '4',
+		look: 'modern',
+		image: './assets/attire/4_12_m.jpg',
+	},
+	{
+		name: '4_13_c',
+		temperature: '4',
+		look: 'casual',
+		image: './assets/attire/4_13_c.jpg',
+	},
+	{
+		name: '4_14_c',
+		temperature: '4',
+		look: 'casual',
+		image: './assets/attire/4_14_c.jpg',
+	},
+	{
+		name: '4_15_c',
+		temperature: '4',
+		look: 'casual',
+		image: './assets/attire/4_15_c.jpg',
+	},
+	{
+		name: '4_16_c',
+		temperature: '4',
+		look: 'casual',
+		image: './assets/attire/4_16_c.jpg',
+	},
+	{
+		name: '4_17_r',
+		temperature: '4',
+		look: 'romantic',
+		image: './assets/attire/4_17_r.jpg',
+	},
+	{
+		name: '5_1_c',
+		temperature: '5',
+		look: 'casual',
+		image: './assets/attire/5_1_c.jpg',
+	},
+	{
+		name: '5_2_m',
+		temperature: '11',
+		look: 'modern',
+		image: './assets/attire/5_2_m.jpg',
+	},
+	{
+		name: '5_3_m',
+		temperature: '11',
+		look: 'modern',
+		image: './assets/attire/5_3_m.jpg',
+	},
+	{
+		name: '5_4_c',
+		temperature: '11',
+		look: 'casual',
+		image: './assets/attire/5_4_c.jpg',
+	},
+	{
+		name: '5_5_c',
+		temperature: '5',
+		look: 'casual',
+		image: './assets/attire/5_5_c.jpg',
+	},
+	{
+		name: '5_6_m',
+		temperature: '5',
+		look: 'romantic',
+		image: './assets/attire/5_6_m.jpg',
+	},
+	{
+		name: '5_7_c',
+		temperature: '5',
+		look: 'casual',
+		image: './assets/attire/5_7_c.jpg',
+	},
+	{
+		name: '5_8_c',
+		temperature: '5',
+		look: 'casual',
+		image: './assets/attire/5_8_c.jpg',
+	},
+	{
+		name: '5_9_c',
+		temperature: '5',
+		look: 'casual',
+		image: './assets/attire/5_9_c.jpg',
+	},
+	{
+		name: '5_10_m',
+		temperature: '5',
+		look: 'modern',
+		image: './assets/attire/5_10_m.jpg',
+	},
+	{
+		name: '5_11_m',
+		temperature: '5',
+		look: 'modern',
+		image: './assets/attire/5_11_m.jpg',
+	},
+	{
+		name: '5_12_c',
+		temperature: '5',
+		look: 'casual',
+		image: './assets/attire/5_12_c.jpg',
+	},
+	{
+		name: '5_13_c',
+		temperature: '5',
+		look: 'casual',
+		image: './assets/attire/5_13_c.jpg',
+	},
+	{
+		name: '5_14_a',
+		temperature: '5',
+		look: 'amekaji',
+		image: './assets/attire/5_14_a.jpg',
+	},
+	{
+		name: '5_15_c',
+		temperature: '5',
+		look: 'casual',
+		image: './assets/attire/5_15_c.jpg',
+	},
+	{
+		name: '5_16_m',
+		temperature: '5',
+		look: 'modern',
+		image: './assets/attire/5_16_m.jpg',
+	},
+	{
+		name: '9_1_c',
+		temperature: '9',
+		look: 'casual',
+		image: './assets/attire/9_1_c.jpg',
+	},
+	{
+		name: '9_2_c',
+		temperature: '9',
+		look: 'casual',
+		image: './assets/attire/9_2_c.jpg',
+	},
+	{
+		name: '9_3_c',
+		temperature: '9',
+		look: 'casual',
+		image: './assets/attire/9_3_c.jpg',
+	},
+	{
+		name: '9_4_m',
+		temperature: '9',
+		look: 'modern',
+		image: './assets/attire/9_4_m.jpg',
+	},
+	{
+		name: '9_5_s',
+		temperature: '9',
+		look: 'street',
+		image: './assets/attire/9_5_s.jpg',
+	},
+	{
+		name: '9_6_c',
+		temperature: '9',
+		look: 'casual',
+		image: './assets/attire/9_6_c.jpg',
+	},
+	{
+		name: '9_7_m',
+		temperature: '9',
+		look: 'modern',
+		image: './assets/attire/9_7_m.jpg',
+	},
+	{
+		name: '9_8_c',
+		temperature: '9',
+		look: 'casual',
+		image: './assets/attire/9_8_c.jpg',
+	},
+	{
+		name: '9_9_s',
+		temperature: '9',
+		look: 'street',
+		image: './assets/attire/9_9_s.jpg',
+	},
+	{
+		name: '9_10_c',
+		temperature: '9',
+		look: 'casual',
+		image: './assets/attire/9_10_c.jpg',
+	},
+	{
+		name: '9_11_m',
+		temperature: '9',
+		look: 'modern',
+		image: './assets/attire/9_11_m.jpg',
+	},
+	{
+		name: '9_12_c',
+		temperature: '9',
+		look: 'casual',
+		image: './assets/attire/9_12_c.jpg',
+	},
+	{
+		name: '9_13_c',
+		temperature: '9',
+		look: 'casual',
+		image: './assets/attire/9_13_c.jpg',
+	},
+	{
+		name: '9_14_a',
+		temperature: '9',
+		look: 'amekaji',
+		image: './assets/attire/9_14_a.jpg',
+	},
+	{
+		name: '9_15_c',
+		temperature: '9',
+		look: 'casual',
+		image: './assets/attire/9_15_c.jpg',
+	},
+	{
+		name: '9_16_a',
+		temperature: '-1',
+		look: 'amekaji',
+		image: './assets/attire/9_16_a.jpg',
+	},
+	{
+		name: '12_1_r',
+		temperature: '12',
+		look: 'amekaji',
+		image: './assets/attire/12_1_r.jpg',
+	},
+	{
+		name: '12_2_r',
+		temperature: '12',
+		look: 'romatic',
+		image: './assets/attire/12_2_r.jpg',
+	},
+	{
+		name: '12_3_r',
+		temperature: '12',
+		look: 'romatic',
+		image: './assets/attire/12_3_r.jpg',
+	},
+	{
+		name: '12_4_s',
+		temperature: '12',
+		look: 'street',
+		image: './assets/attire/12_4_s.jpg',
+	},
+	{
+		name: '12_5_s',
+		temperature: '12',
+		look: 'street',
+		image: './assets/attire/12_5_s.jpg',
+	},
+	{
+		name: '12_6_s',
+		temperature: '12',
+		look: 'street',
+		image: './assets/attire/12_6_s.jpg',
+	},
+	{
+		name: '12_7_s',
+		temperature: '12',
+		look: 'street',
+		image: './assets/attire/12_7_s.jpg',
+	},
+	{
+		name: '12_8_s',
+		temperature: '12',
+		look: 'romatic',
+		image: './assets/attire/12_2_r.jpg',
+	},
+	{
+		name: '12_9_s',
+		temperature: '12',
+		look: 'street',
+		image: './assets/attire/12_9_s.jpg',
+	},
+	{
+		name: '12_10_s',
+		temperature: '12',
+		look: 'street',
+		image: './assets/attire/12_10_s.jpg',
+	},
+	{
+		name: '12_11_s',
+		temperature: '12',
+		look: 'street',
+		image: './assets/attire/12_11_s.jpg',
+	},
+	{
+		name: '17_1_r',
+		temperature: '17',
+		look: 'romantic',
+		image: './assets/attire/17_1_r.jpg',
+	},
+	{
+		name: '17_2_r',
+		temperature: '17',
+		look: 'romantic',
+		image: './assets/attire/17_2_r.jpg',
+	},
+	{
+		name: '17_3_r',
+		temperature: '17',
+		look: 'romantic',
+		image: './assets/attire/17_3_r.jpg',
+	},
+	{
+		name: '17_4_s',
+		temperature: '17',
+		look: 'street',
+		image: './assets/attire/17_4_s.jpg',
+	},
+	{
+		name: '17_5_s',
+		temperature: '17',
+		look: 'street',
+		image: './assets/attire/17_5_s.jpg',
+	},
+	{
+		name: '17_6_r',
+		temperature: '17',
+		look: 'romantic',
+		image: './assets/attire/17_6_r.jpg',
+	},
+	{
+		name: '17_7_r',
+		temperature: '17',
+		look: 'romantic',
+		image: './assets/attire/17_7_r.jpg',
+	},
+	{
+		name: '17_8_s',
+		temperature: '17',
+		look: 'street',
+		image: './assets/attire/17_8_s.jpg',
+	},
+	{
+		name: '20_1_r',
+		temperature: '20',
+		look: 'romantic',
+		image: './assets/attire/20_1_r.jpg',
+	},
+	{
+		name: '20_2_s',
+		temperature: '20',
+		look: 'street',
+		image: './assets/attire/20_2_s.jpg',
+	},
+	{
+		name: '20_3_s',
+		temperature: '20',
+		look: 'street',
+		image: './assets/attire/20_3_s.jpg',
+	},
+	{
+		name: '20_4_r',
+		temperature: '20',
+		look: 'romantic',
+		image: './assets/attire/20_4_r.jpg',
+	},
+	{
+		name: '20_5_r',
+		temperature: '20',
+		look: 'romantic',
+		image: './assets/attire/20_5_r.jpg',
+	},
+	{
+		name: '20_6_c',
+		temperature: '20',
+		look: 'casual',
+		image: './assets/attire/20_6_c.jpg',
+	},
+	{
+		name: '20_7_m',
+		temperature: '20',
+		look: 'modern',
+		image: './assets/attire/20_7_m.jpg',
+	},
+	{
+		name: '20_8_r',
+		temperature: '-4',
+		look: 'romantic',
+		image: './assets/attire/20_8_r.jpg',
+	},
+	{
+		name: '20_9_s',
+		temperature: '-3',
+		look: 'street',
+		image: './assets/attire/20_9_s.jpg',
+	},
+	{
+		name: '23_1_r',
+		temperature: '23',
+		look: 'romatic',
+		image: './assets/attire/23_1_r.jpg',
+	},
+	{
+		name: '23_2_r',
+		temperature: '23',
+		look: 'romatic',
+		image: './assets/attire/23_2_r.jpg',
+	},
+	{
+		name: '23_3_r',
+		temperature: '1',
+		look: 'romatic',
+		image: './assets/attire/23_3_r.jpg',
+	},
+	{
+		name: '23_4_r',
+		temperature: '2',
+		look: 'romatic',
+		image: './assets/attire/23_4_r.jpg',
+	},
 ];
 
-
-//-------------------------------------------날씨 구역 ----------------------
-
-// Openweather API 날짜 정보 받을 수 있는 API키
-const API_KEY = "d1f4c51106f97ceb117112414f8e5794";
-
-// 브라우저에서 제공하는 위치정보 받는 기본함수
-// (navigator.geolocation에서 위치정보 받는것을 성공했을 때 실행 하는 함수)
-function onGeoOk(position) {
-  // 위도 경도 변수 지정
-  const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-
-  // Openweather API 날씨 정도 사용을 위한 url 변수 지정 (위도 경도 참조)
-  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&exclude=minutely,hourly&appid=${API_KEY}&units=metric&lang=kr`;
-
-  // 일주일간의 온도 정보 가져오기 위한 Openweather API 서버에 요청
-  fetch(url).then(response => response.json()).then(data => {
-    // 서버로 요청한 데이터를 배열로 담기 위한 빈 배열 지정
-    const tempArray = [];
-    // 날짜 정보 담기 위한 변수 지정
-    var date = '';
-
-    // 날짜 지정
-    date = new Date(data.daily[0].dt * 1000);
-
-    // 날씨 정보 입력하기
-    tempArray.push(data.daily[0].temp.day);
-
-    console.log(Math.round(tempArray[0]));
-
-    // 실시간 온도
-    let apiTemp = Math.round(tempArray[0]);
-
-    // ----------------------------------날씨 구역 ---------------------------
-
-
-
-
-
-    //템플릿 (객체 갯수만큼 템플릿이 반복됨)
-    function itemTemplate(item) {
-      return `
-    <img src="${item.image}">
-    `
-    }
-
-
-    //셀렉트박스
-    document.querySelector(".select").innerHTML = `
-<select name="" id="selectBox" onchange="selects(this)">
-    <option value="9">${apiTemp}</option>
-    <option value="5">8~5℃</option>
-    <option value="4">4℃↓</option>
-</select>
-`
-
-    //filter
-    var four = items.filter(function (element) {
-      return element.temperature == "4";
-    });
-
-    var five = items.filter(function (element) {
-      return element.temperature == "5";
-    });
-
-    var nine = items.filter(function (element) {
-      return element.temperature == "9";
-    });
-
-
-
-    /*-------------- 성진 추가 ------------- */
-    if (4 < apiTemp < 10) {
-      document.getElementById("imgBox").innerHTML = `
-      ${four.map(itemTemplate).join("")}
-      `}
-
-
-
-
-    //imgbox안에 들어갈 내용 (템플릿과 연결)
-    //temperature 셀렉트별 이미지 띄우기
-    function selects(s) {
-      if (s.value == 4) {
-        document.getElementById("imgBox").innerHTML = `
-        ${four.map(itemTemplate).join("")}
-        `
-      } else if (s.value == 5) {
-        document.getElementById("imgBox").innerHTML = `
-        ${five.map(itemTemplate).join("")}
-        `
-      } else {
-        document.getElementById("imgBox").innerHTML = `
-        ${nine.map(itemTemplate).join("")}
-        `
-      }
-    }
-
-  });
-
+//템플릿 (imgBox안에 이미지 띄우기)
+function itemTemplate(item) {
+	return `
+  <img src="${item.image}">
+  `;
 }
 
+//api에서 기온값 가져오기
+const Temp = localStorage.getItem('temp');
+console.log(Temp);
 
+//select에 실시간 기온값 넣은 option추가
+document.getElementById('nowTemp').innerHTML = `
+<option value="">${Temp}</option>
+`;
 
+//select에서 실시간 기온 가져오기
+let tempOptions = document.getElementById('nowTemp');
+console.log(tempOptions.text);
 
+//실시간 온도와 json온도가 같은것만 필터링
+const filtered = items.filter((i) => i.temperature == nowTemp.text);
 
-// --------------------
-// navigator.geolocation.getCurrentPosition에서 위치 정보를 받는데 실패 했을 때,
-// 실행 할 함수
-function onGeoError() {
-  alert("위치정보를 찾을 수 없습니다.")
+if (tempOptions.text < 30 || tempOptions.text > 30) {
+	document.getElementById('imgBox').innerHTML = `
+    ${filtered.map(itemTemplate).join('')}
+    `;
+	console.log(filtered);
 }
 
-// navigator.geolocation.getCurrentPosition(위치 정보 받는데 성공했을 때 실행 할 함수, 실패 했을 때 실행 할 함수)
-navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+//셀렉트(기온)별 이미지 띄우기
+function selects(s) {
+	filtered_items = items.filter((i) => i.temperature == s.value);
+	document.getElementById('imgBox').innerHTML = `
+    ${filtered_items.map(itemTemplate).join('')}
+    `;
+}
+
+//버튼별(룩)별 이미지 띄우기
+function btnLook(s) {
+	filter_look = filtered_items.filter((i) => i.look == s.value);
+	document.getElementById('imgBox').innerHTML = `
+    ${filter_look.map(itemTemplate).join('')}
+    `;
+}
