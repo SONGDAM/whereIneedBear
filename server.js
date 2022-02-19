de06a4e2724a277d35af3b58a85990cf6465b45e
SONGDAM <herb7761@gmail.com>
2022-02-19 23:43:51 +0900

    express

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
-
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = 8080;
 
-app.use(express.static(__dirname + "/src"));
+app.use(express.static(__dirname + '/src'));
 
-
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "index.html");
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + 'index.html');
 });
 
-
 app.listen(PORT, () => {
-  console.log(`Listen : ${PORT}`);
+	console.log(`Listen : ${PORT}`);
 });
---
+++ src/JS/admin.js
@@ -0,0 +1,5 @@
+'use strict';
+
+document.querySelector('.input').addEventListener('input', (e) => {
+	console.log(e.target.value);
+});
