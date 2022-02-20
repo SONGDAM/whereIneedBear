14a6bfeded0fd05aeb14e43017c21aa67c5027ed
bitnal-h <91711520+bitnal-h@users.noreply.github.com>
2022-02-20 12:28:14 +0900

    0220
    0220

--- src/JS/my-img.js
+++ src/JS/my-img.js
@@ -1,313 +1,19 @@
-<<<<<<< Updated upstream
-const items = [
-  {
-    name: "4_img_1",
-    temperature: "4",
-    image: "../assets/attire/4_img_1.jpg",
-  },
-  {
-    name: "4_img_2",
-    temperature: "4",
-    image: "../assets/attire/4_img_2.jpg",
-  },
-  {
-    name: "4_img_3",
-    temperature: "4",
-    image: "../assets/attire/4_img_3.jpg",
-  },
-  {
-    name: "4_img_4",
-    temperature: "4",
-    image: "../assets/attire/4_img_4.jpg",
-  },
-  {
-    name: "4_img_5",
-    temperature: "4",
-    image: "../assets/attire/4_img_5.jpg",
-  },
-  {
-    name: "4_img_6",
-    temperature: "4",
-    image: "../assets/attire/4_img_6.jpg",
-  },
-  {
-    name: "4_img_7",
-    temperature: "4",
-    image: "../assets/attire/4_img_7.jpg",
-  },
-  {
-    name: "4_img_8",
-    temperature: "4",
-    image: "../assets/attire/4_img_8.jpg",
-  },
-  {
-    name: "4_img_9",
-    temperature: "4",
-    image: "../assets/attire/4_img_9.jpg",
-  },
-  {
-    name: "4_img_10",
-    temperature: "4",
-    image: "../assets/attire/4_img_10.jpg",
-  },
-  {
-    name: "4_img_11",
-    temperature: "4",
-    image: "../assets/attire/4_img_11.jpg",
-  },
-  {
-    name: "4_img_12",
-    temperature: "4",
-    image: "../assets/attire/4_img_12.jpg",
-  },
-  {
-    name: "4_img_13",
-    temperature: "4",
-    image: "../assets/attire/4_img_13.jpg",
-  },
-  {
-    name: "4_img_14",
-    temperature: "4",
-    image: "../assets/attire/4_img_14.jpg",
-  },
-  {
-    name: "4_img_15",
-    temperature: "4",
-    image: "../assets/attire/4_img_15.jpg",
-  },
-  {
-    name: "4_img_16",
-    temperature: "4",
-    image: "../assets/attire/4_img_16.jpg",
-  },
-  {
-    name: "5_img_1",
-    temperature: "5",
-    image: "../assets/attire/5_img_1.jpg",
-  },
-  {
-    name: "5_img_2",
-    temperature: "5",
-    image: "../assets/attire/5_img_2.jpg",
-  },
-  {
-    name: "5_img_3",
-    temperature: "5",
-    image: "../assets/attire/5_img_3.jpg",
-  },
-  {
-    name: "5_img_4",
-    temperature: "5",
-    image: "../assets/attire/5_img_4.jpg",
-  },
-  {
-    name: "5_img_5",
-    temperature: "5",
-    image: "../assets/attire/5_img_5.jpg",
-  },
-  {
-    name: "5_img_6",
-    temperature: "5",
-    image: "../assets/attire/5_img_6.jpg",
-  },
-  {
-    name: "5_img_7",
-    temperature: "5",
-    image: "../assets/attire/5_img_7.jpg",
-  },
-  {
-    name: "5_img_8",
-    temperature: "5",
-    image: "../assets/attire/5_img_8.jpg",
-  },
-  {
-    name: "5_img_9",
-    temperature: "5",
-    image: "../assets/attire/5_img_9.jpg",
-  },
-  {
-    name: "5_img_10",
-    temperature: "5",
-    image: "../assets/attire/5_img_10.jpg",
-  },
-  {
-    name: "5_img_11",
-    temperature: "5",
-    image: "../assets/attire/5_img_11.jpg",
-  },
-  {
-    name: "5_img_12",
-    temperature: "5",
-    image: "../assets/attire/5_img_12.jpg",
-  },
-  {
-    name: "5_img_13",
-    temperature: "5",
-    image: "../assets/attire/5_img_13.jpg",
-  },
-  {
-    name: "5_img_14",
-    temperature: "5",
-    image: "../assets/attire/5_img_14.jpg",
-  },
-  {
-    name: "5_img_15",
-    temperature: "5",
-    image: "../assets/attire/5_img_15.jpg",
-  },
-  {
-    name: "5_img_16",
-    temperature: "5",
-    image: "../assets/attire/5_img_16.jpg",
-  },
-  {
-    name: "9_img_1",
-    temperature: "9",
-    image: "../assets/attire/9_img_1.jpg",
-  },
-  {
-    name: "9_img_2",
-    temperature: "9",
-    image: "../assets/attire/9_img_2.jpg",
-  },
-  {
-    name: "9_img_3",
-    temperature: "9",
-    image: "../assets/attire/9_img_3.jpg",
-  },
-  {
-    name: "9_img_4",
-    temperature: "9",
-    image: "../assets/attire/9_img_4.jpg",
-  },
-  {
-    name: "9_img_5",
-    temperature: "9",
-    image: "../assets/attire/9_img_5.jpg",
-  },
-  {
-    name: "9_img_6",
-    temperature: "9",
-    image: "../assets/attire/9_img_6.jpg",
-  },
-  {
-    name: "9_img_7",
-    temperature: "9",
-    image: "../assets/attire/9_img_7.jpg",
-  },
-  {
-    name: "9_img_8",
-    temperature: "9",
-    image: "../assets/attire/9_img_8.jpg",
-  },
-  {
-    name: "9_img_9",
-    temperature: "9",
-    image: "../assets/attire/9_img_9.jpg",
-  },
-  {
-    name: "9_img_10",
-    temperature: "9",
-    image: "../assets/attire/9_img_10.jpg",
-  },
-  {
-    name: "9_img_11",
-    temperature: "9",
-    image: "../assets/attire/9_img_11.jpg",
-  },
-  {
-    name: "9_img_12",
-    temperature: "9",
-    image: "../assets/attire/9_img_12.jpg",
-  },
-  {
-    name: "9_img_13",
-    temperature: "9",
-    image: "../assets/attire/9_img_13.jpg",
-  },
-  {
-    name: "9_img_14",
-    temperature: "9",
-    image: "../assets/attire/9_img_14.jpg",
-  },
-  {
-    name: "9_img_15",
-    temperature: "9",
-    image: "../assets/attire/9_img_15.jpg",
-  },
-  {
-    name: "9_img_16",
-    temperature: "9",
-    image: "../assets/attire/9_img_16.jpg",
-  },
-];
-
-//템플릿 (객체 갯수만큼 템플릿이 반복됨)
-function itemTemplate(item) {
-  return `
-    <img src="${item.image}">
-    `
-}
-
-
-//셀렉트박스
- document.querySelector(".select").innerHTML = `
-<select name="" id="selectBox" onchange="selects(this)">
-    <option value="9">11~9℃</option>
-    <option value="5">8~5℃</option>
-    <option value="4">4℃↓</option>
-</select>
- `
-
-//filter
-  var four = items.filter(function(element){
-      return element.temperature =="4";
-  });
-
-  var five = items.filter(function(element){
-      return element.temperature =="5";
-  });
-
-  var nine = items.filter(function(element){
-      return element.temperature =="9";
-  });
-
-
-
-//imgbox안에 들어갈 내용 (템플릿과 연결)
-//temperature 셀렉트별 이미지 띄우기
-function selects(s){
-  if(s.value==4){
-    document.getElementById("imgBox").innerHTML = `
-        ${four.map(itemTemplate).join("")}
-        `
-  }else if(s.value==5){
-    document.getElementById("imgBox").innerHTML = `
-        ${five.map(itemTemplate).join("")}
-        `
-  }else{
-    document.getElementById("imgBox").innerHTML = `
-        ${nine.map(itemTemplate).join("")}
-        `
-  }
-}
-=======
 const items = [
 	{
 		name: '4_1_c',
-		temperature: '-4',
+		temperature: '4',
 		look: 'casual',
 		image: './assets/attire/4_1_c.jpg',
 	},
 	{
 		name: '4_2_c',
-		temperature: '-4',
+		temperature: '4',
 		look: 'casual',
 		image: './assets/attire/4_2_c.jpg',
 	},
 	{
 		name: '4_3_m',
-		temperature: '-4',
+		temperature: '4',
 		look: 'modern',
 		image: './assets/attire/4_3_m.jpg',
 	},
@@ -403,19 +109,19 @@ const items = [
 	},
 	{
 		name: '5_2_m',
-		temperature: '11',
+		temperature: '5',
 		look: 'modern',
 		image: './assets/attire/5_2_m.jpg',
 	},
 	{
 		name: '5_3_m',
-		temperature: '11',
+		temperature: '5',
 		look: 'modern',
 		image: './assets/attire/5_3_m.jpg',
 	},
 	{
 		name: '5_4_c',
-		temperature: '11',
+		temperature: '5',
 		look: 'casual',
 		image: './assets/attire/5_4_c.jpg',
 	},
@@ -745,13 +451,13 @@ const items = [
 	},
 	{
 		name: '20_8_r',
-		temperature: '-4',
+		temperature: '20',
 		look: 'romantic',
 		image: './assets/attire/20_8_r.jpg',
 	},
 	{
 		name: '20_9_s',
-		temperature: '-3',
+		temperature: '20',
 		look: 'street',
 		image: './assets/attire/20_9_s.jpg',
 	},
@@ -769,13 +475,13 @@ const items = [
 	},
 	{
 		name: '23_3_r',
-		temperature: '1',
+		temperature: '23',
 		look: 'romatic',
 		image: './assets/attire/23_3_r.jpg',
 	},
 	{
 		name: '23_4_r',
-		temperature: '3',
+		temperature: '23',
 		look: 'romatic',
 		image: './assets/attire/23_4_r.jpg',
 	},
@@ -798,63 +504,6 @@ document.getElementById('tempSelect').innerHTML += `
  <option  id="nowTemp" value="${Temp}"  selected>Now: ${Temp}°</option>
  `;
 
-// //select에 실시간 기온값 넣은 option추가
-// let option = document.createElement("option");             //option요소 추가
-// option.id = "nowTemp";                                  //option요소에 id 추가
-// option.value =`${Temp}`;                           //option요소에 value = 실시간 온도로 추가
-// let optionText =  document.createTextNode(`Now: ${Temp}°`); //option 텍스트 추가
-// option.appendChild(optionText);                        //option 요소에 text 합침
-// document.getElementById("tempSelect").prepend(option);      //tempSelect에 제일 첫번째 순서로 추가
-
-
-
-//select 실시간 옵션에서 기온값만 문자 잘라내기
-// let nowTempText = document.getElementById('nowTemp').text;
-// let nowTemp = nowTempText.substring(5,nowTempText.indexOf('°'));
-// let Tselect = document.getElementById("tempSelect");
-// let nowTemp = Tselect.options[Tselect.selectedIndex].value;
-// let nowTemp = document.getElementById('nowTemp').value;
-// console.log("nowTemp:"+nowTemp);
-
-
-//  function selects2(s){
-
-// // 메인 페이지에 데이터 생성 함수
-//     if(Temp <= 4) {
-//         nowTemp = 4;
-//     } else if (Temp >= 4) {
-//         nowTemp = 8;
-//     } else if (Temp >= 11) {
-//         nowTemp = 11;
-//     } else if (Temp >= 16) {
-//         nowTemp = 16;
-//     } else if (Temp >= 19) {
-//         nowTemp = 19;
-//     } else if (Temp >= 22) {
-//         nowTemp = 22;
-//     } else if (Temp >= 27) {
-//         nowTemp = 27;
-//     }
-
-//    //실시간 온도와 json온도가 같은것만 필터링
-
-//    const filtered = items.filter((i) => i.temperature == nowTemp);
-
-//    document.getElementById('imgItem').innerHTML = `
-//    ${filtered.map(itemTemplate).join('')}
-//    `;
-// }
-// console.log(filtered);
-// console.log("nowTemp: " + nowTemp);
-
-// if()
-//value값 변경 테스트
-// let nowTemp = document.getElementById("tempSelect").options[7].value;
-// console.log("ggggggg:"+nowTemp);
-// if(Temp <=4 ){
-//    nowTemp = 4;
-// }
-// console.log("aaaaa:"+nowTemp);
 
 let nowTemp = document.getElementById("tempSelect").options[7].value;
 console.log("실시간 value:" + nowTemp);
@@ -912,4 +561,3 @@ function btnLook(s) {
 	 ${filter_look.map(itemTemplate).join('')}
 	 `;
 }
->>>>>>> Stashed changes
