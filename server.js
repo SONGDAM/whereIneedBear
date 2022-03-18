bad66d773bb8ac107505cacf8f2ec66ac3eb4ae3
SONGDAM <herb7761@gmail.com>
2022-03-19 02:19:55 +0900

    update admin template pug & make login page

--- server.js
+++ server.js
@@ -1,13 +1,42 @@
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const multer = require('multer');
+const PORT = 4000;
+const path = require('path');
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+      cb(null, new Date().valueOf() + path.extname(file.originalname));
+    },
+  }),
+});
+
+app.set('views', process.cwd() + '/views');
+
+app.set('view engine', 'pug');
+
+app.get('/admin', (req, res) => {
+  res.render('admin');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/up', upload.array('img'), (req, res) => {
+  console.log(req.files);
+  res.send('<p class="up">successed!!</p>');
+});
 
 app.use(express.static(__dirname + '/src'));
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + 'index.html');
+  res.sendFile(__dirname + 'index.html');
 });
 
 app.listen(PORT, () => {
-	console.log(`Listen : ${PORT}`);
+  console.log(`Listen : ${PORT}`);
 });
